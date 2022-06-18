ac74761d08bd36f663d6b9fa9715a7143e4c8290
nanjizal <nanjizal@users.noreply.github.com>
2022-06-18 08:37:24 +0100

    Create Doc.hx

---
+++ src/iterOther/upkeep/Doc.hx
@@ -0,0 +1,14 @@
+package iterOther.upkeep;
+import iterOther.*;
+
+// Haxe Dox documentation class
+
+@:dox(hide)
+class Doc {
+    public static function main(){
+        
+    }
+    public function new(){
+        
+    }
+}
